b61c9db49ab2b27498502153d15e04cb8cd3d28a
Cody Scott <cs475x@icloud.com>
2020-12-21 15:45:34 -0800

    Simplify Palette

--- lib/static/palette.ts
+++ lib/static/palette.ts
@@ -1,52 +1,45 @@
-class PlayerColors {
-  public static readonly red: [198, 17, 17, 255];
-  public static readonly blue: [19, 46, 210, 255];
-  public static readonly green: [17, 128, 45, 255];
-  public static readonly pink: [238, 84, 187, 255];
-  public static readonly orange: [240, 125, 13, 255];
-  public static readonly yellow: [246, 246, 87, 255];
-  public static readonly black: [63, 71, 78, 255];
-  public static readonly white: [215, 225, 241, 255];
-  public static readonly purple: [107, 47, 188, 255];
-  public static readonly brown: [113, 73, 30, 255];
-  public static readonly cyan: [56, 255, 221, 255];
-  public static readonly lime: [80, 240, 57, 255];
-}
+import { PlayerColor } from "../types/enums";
 
-class PlayerShadowColors {
-  public static readonly red: [122, 8, 56, 255];
-  public static readonly blue: [9, 21, 142, 255];
-  public static readonly green: [10, 77, 46, 255];
-  public static readonly pink: [172, 43, 174, 255];
-  public static readonly orange: [180, 62, 21, 255];
-  public static readonly yellow: [195, 136, 34, 255];
-  public static readonly black: [30, 31, 38, 255];
-  public static readonly white: [132, 149, 192, 255];
-  public static readonly purple: [59, 23, 124, 255];
-  public static readonly brown: [94, 38, 21, 255];
-  public static readonly cyan: [36, 169, 191, 255];
-  public static readonly lime: [21, 168, 66, 255];
-}
+type Color = readonly [red: number, green: number, blue: number, alpha: number];
+
+type PlayerColorValues = {
+  [key in PlayerColor]: Readonly<{
+    light: Color;
+    dark: Color;
+  }>;
+};
 
 export class Palette {
-  public static readonly disabledGrey = [76, 76, 76, 255];
-  public static readonly disabledColor = [255, 255, 255, 76];
-  public static readonly enabledColor = [255, 255, 255, 255];
-  public static readonly black = [0, 0, 0, 255];
-  public static readonly clearWhite = [255, 255, 255, 0];
-  public static readonly halfWhite = [255, 255, 255, 127];
-  public static readonly white = [255, 255, 255, 255];
-  public static readonly lightBlue = [127, 127, 255, 255];
-  public static readonly blue = [51, 51, 255, 255];
-  public static readonly orange = [255, 153, 0.005, 255];
-  public static readonly purple = [153, 25, 153, 255];
-  public static readonly brown = [183, 109, 28, 255];
-  public static readonly crewmateBlue = [140, 255, 255, 255];
-  public static readonly impostorRed = [255, 25, 25, 255];
+  public static readonly disabledGrey: Color = [76, 76, 76, 255];
+  public static readonly disabledColor: Color = [255, 255, 255, 76];
+  public static readonly enabledColor: Color = [255, 255, 255, 255];
+  public static readonly black: Color = [0, 0, 0, 255];
+  public static readonly clearWhite: Color = [255, 255, 255, 0];
+  public static readonly halfWhite: Color = [255, 255, 255, 127];
+  public static readonly white: Color = [255, 255, 255, 255];
+  public static readonly lightBlue: Color = [127, 127, 255, 255];
+  public static readonly blue: Color = [51, 51, 255, 255];
+  public static readonly orange: Color = [255, 153, 0.005, 255];
+  public static readonly purple: Color = [153, 25, 153, 255];
+  public static readonly brown: Color = [183, 109, 28, 255];
+  public static readonly crewmateBlue: Color = [140, 255, 255, 255];
+  public static readonly impostorRed: Color = [255, 25, 25, 255];
 
-  public static readonly playerColors = PlayerColors;
+  public static readonly playerBody: Readonly<PlayerColorValues> = {
+    [PlayerColor.Red]: { light: [198, 17, 17, 255], dark: [122, 8, 56, 255] },
+    [PlayerColor.Blue]: { light: [19, 46, 210, 255], dark: [9, 21, 142, 255] },
+    [PlayerColor.Green]: { light: [17, 128, 45, 255], dark: [10, 77, 46, 255] },
+    [PlayerColor.Pink]: { light: [238, 84, 187, 255], dark: [172, 43, 174, 255] },
+    [PlayerColor.Orange]: { light: [240, 125, 13, 255], dark: [180, 62, 21, 255] },
+    [PlayerColor.Yellow]: { light: [246, 246, 87, 255], dark: [195, 136, 34, 255] },
+    [PlayerColor.Black]: { light: [63, 71, 78, 255], dark: [30, 31, 38, 255] },
+    [PlayerColor.White]: { light: [215, 225, 241, 255], dark: [132, 149, 192, 255] },
+    [PlayerColor.Purple]: { light: [107, 47, 188, 255], dark: [59, 23, 124, 255] },
+    [PlayerColor.Brown]: { light: [113, 73, 30, 255], dark: [94, 38, 21, 255] },
+    [PlayerColor.Cyan]: { light: [56, 255, 221, 255], dark: [36, 169, 191, 255] },
+    [PlayerColor.Lime]: { light: [80, 240, 57, 255], dark: [21, 168, 66, 255] },
+    [PlayerColor.ForteGreen]: { light: [29, 152, 83, 255], dark: [18, 63, 27, 255] },
+  };
 
-  public static readonly playerShadowColors = PlayerShadowColors;
-
-  public static readonly visorColor = [149, 202, 220, 255];
+  public static readonly playerVisor: Color = [149, 202, 220, 255];
 }
