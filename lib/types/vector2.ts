b80b857b5be2bcd0b2b69f1b4b57c9918476193b
Cody Scott <cs475x@icloud.com>
2021-01-18 15:51:08 -0800

    Add basic math operations to Vector2

--- lib/types/vector2.ts
+++ lib/types/vector2.ts
@@ -22,4 +22,108 @@ export class Vector2 {
     return isFloatEqual(this.x, other.x, epsilon)
         && isFloatEqual(this.y, other.y, epsilon);
   }
+
+  clone(): Vector2 {
+    return new Vector2(this.x, this.y);
+  }
+
+  add(value: Vector2): Vector2;
+  add(x: number, y: number): Vector2;
+  add(x: number | Vector2, y?: number): Vector2 {
+    if (x instanceof Vector2) {
+      y = x.y;
+      x = x.x;
+    } else {
+      y = y ?? 0;
+    }
+
+    return new Vector2(this.x + x, this.y + y);
+  }
+
+  addX(value: number | Vector2): Vector2 {
+    value = value instanceof Vector2 ? value.x : value;
+
+    return new Vector2(this.x + value, this.y);
+  }
+
+  addY(value: number | Vector2): Vector2 {
+    value = value instanceof Vector2 ? value.y : value;
+
+    return new Vector2(this.x, this.y + value);
+  }
+
+  subtract(value: Vector2): Vector2;
+  subtract(x: number, y: number): Vector2;
+  subtract(x: number | Vector2, y?: number): Vector2 {
+    if (x instanceof Vector2) {
+      y = x.y;
+      x = x.x;
+    } else {
+      y = y ?? 0;
+    }
+
+    return new Vector2(this.x - x, this.y - y);
+  }
+
+  subtractX(value: number | Vector2): Vector2 {
+    value = value instanceof Vector2 ? value.x : value;
+
+    return new Vector2(this.x - value, this.y);
+  }
+
+  subtractY(value: number | Vector2): Vector2 {
+    value = value instanceof Vector2 ? value.y : value;
+
+    return new Vector2(this.x, this.y - value);
+  }
+
+  multiply(value: Vector2): Vector2;
+  multiply(x: number, y: number): Vector2;
+  multiply(x: number | Vector2, y?: number): Vector2 {
+    if (x instanceof Vector2) {
+      y = x.y;
+      x = x.x;
+    } else {
+      y = y ?? 0;
+    }
+
+    return new Vector2(this.x * x, this.y * y);
+  }
+
+  multiplyX(value: number | Vector2): Vector2 {
+    value = value instanceof Vector2 ? value.x : value;
+
+    return new Vector2(this.x * value, this.y);
+  }
+
+  multiplyY(value: number | Vector2): Vector2 {
+    value = value instanceof Vector2 ? value.y : value;
+
+    return new Vector2(this.x, this.y * value);
+  }
+
+  divide(value: Vector2): Vector2;
+  divide(x: number, y: number): Vector2;
+  divide(x: number | Vector2, y?: number): Vector2 {
+    if (x instanceof Vector2) {
+      y = x.y;
+      x = x.x;
+    } else {
+      y = y ?? 0;
+    }
+
+    return new Vector2(this.x / x, this.y / y);
+  }
+
+  divideX(value: number | Vector2): Vector2 {
+    value = value instanceof Vector2 ? value.x : value;
+
+    return new Vector2(this.x / value, this.y);
+  }
+
+  divideY(value: number | Vector2): Vector2 {
+    value = value instanceof Vector2 ? value.y : value;
+
+    return new Vector2(this.x, this.y / value);
+  }
 }
--- test/types/vector2.ts
+++ test/types/vector2.ts
@@ -8,3 +8,155 @@ test("checks equality with another vector2", t => {
   t.true(one.equals(two));
   t.false(one.equals(two, 0.0001));
 });
+
+test("it can clone itself", t => {
+  const original = new Vector2(69, 420);
+  const clone = original.clone();
+
+  t.is(clone.x, original.x);
+  t.is(clone.y, original.y);
+});
+
+test("it can add another Vector2", t => {
+  const one = new Vector2(1, 2);
+  const two = new Vector2(3, 4);
+  const resultOne = one.add(two);
+  const resultTwo = one.add(3, 4);
+
+  t.is(resultOne.x, 4);
+  t.is(resultOne.y, 6);
+  t.is(resultTwo.x, 4);
+  t.is(resultTwo.y, 6);
+});
+
+test("it can add to the X value", t => {
+  const one = new Vector2(1, 2);
+  const two = new Vector2(3, 4);
+  const resultOne = one.addX(two);
+  const resultTwo = one.addX(3);
+
+  t.is(resultOne.x, 4);
+  t.is(resultOne.y, 2);
+  t.is(resultTwo.x, 4);
+  t.is(resultTwo.y, 2);
+});
+
+test("it can add to the Y value", t => {
+  const one = new Vector2(1, 2);
+  const two = new Vector2(3, 4);
+  const resultOne = one.addY(two);
+  const resultTwo = one.addY(4);
+
+  t.is(resultOne.x, 1);
+  t.is(resultOne.y, 6);
+  t.is(resultTwo.x, 1);
+  t.is(resultTwo.y, 6);
+});
+
+test("it can subtract another Vector2", t => {
+  const one = new Vector2(1, 2);
+  const two = new Vector2(3, 4);
+  const resultOne = one.subtract(two);
+  const resultTwo = one.subtract(3, 4);
+
+  t.is(resultOne.x, -2);
+  t.is(resultOne.y, -2);
+  t.is(resultTwo.x, -2);
+  t.is(resultTwo.y, -2);
+});
+
+test("it can subtract from the X value", t => {
+  const one = new Vector2(1, 2);
+  const two = new Vector2(3, 4);
+  const resultOne = one.subtractX(two);
+  const resultTwo = one.subtractX(3);
+
+  t.is(resultOne.x, -2);
+  t.is(resultOne.y, 2);
+  t.is(resultTwo.x, -2);
+  t.is(resultTwo.y, 2);
+});
+
+test("it can subtract from the Y value", t => {
+  const one = new Vector2(1, 2);
+  const two = new Vector2(3, 4);
+  const resultOne = one.subtractY(two);
+  const resultTwo = one.subtractY(4);
+
+  t.is(resultOne.x, 1);
+  t.is(resultOne.y, -2);
+  t.is(resultTwo.x, 1);
+  t.is(resultTwo.y, -2);
+});
+
+test("it can multiply with another Vector2", t => {
+  const one = new Vector2(2, 3);
+  const two = new Vector2(4, 5);
+  const resultOne = one.multiply(two);
+  const resultTwo = one.multiply(4, 5);
+
+  t.is(resultOne.x, 8);
+  t.is(resultOne.y, 15);
+  t.is(resultTwo.x, 8);
+  t.is(resultTwo.y, 15);
+});
+
+test("it can multiply the X value", t => {
+  const one = new Vector2(2, 3);
+  const two = new Vector2(4, 5);
+  const resultOne = one.multiplyX(two);
+  const resultTwo = one.multiplyX(4);
+
+  t.is(resultOne.x, 8);
+  t.is(resultOne.y, 3);
+  t.is(resultTwo.x, 8);
+  t.is(resultTwo.y, 3);
+});
+
+test("it can multiply the Y value", t => {
+  const one = new Vector2(2, 3);
+  const two = new Vector2(4, 5);
+  const resultOne = one.multiplyY(two);
+  const resultTwo = one.multiplyY(5);
+
+  t.is(resultOne.x, 2);
+  t.is(resultOne.y, 15);
+  t.is(resultTwo.x, 2);
+  t.is(resultTwo.y, 15);
+});
+
+test("it can be divided by another Vector2", t => {
+  const one = new Vector2(10, 8);
+  const two = new Vector2(5, 2);
+  const resultOne = one.divide(two);
+  const resultTwo = one.divide(5, 2);
+
+  t.is(resultOne.x, 2);
+  t.is(resultOne.y, 4);
+  t.is(resultTwo.x, 2);
+  t.is(resultTwo.y, 4);
+});
+
+test("it can divide the X value", t => {
+  const one = new Vector2(10, 8);
+  const two = new Vector2(5, 2);
+  const resultOne = one.divideX(two);
+  const resultTwo = one.divideX(5);
+
+  t.is(resultOne.x, 2);
+  t.is(resultOne.y, 8);
+  t.is(resultTwo.x, 2);
+  t.is(resultTwo.y, 8);
+});
+
+test("it can divide the Y value", t => {
+  const one = new Vector2(10, 8);
+  const two = new Vector2(5, 2);
+  const resultOne = one.divideY(two);
+  const resultTwo = one.divideY(2);
+
+  t.is(resultOne.x, 10);
+  t.is(resultOne.y, 4);
+  t.is(resultTwo.x, 10);
+  t.is(resultTwo.y, 4);
+});
