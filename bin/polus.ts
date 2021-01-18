b5ba8350b27fa247c6e67dd90577a838e9cc26ad
Cody Scott <cs475x@icloud.com>
2021-01-18 13:57:03 -0800

    Async server start with error handling

--- bin/polus.ts
+++ bin/polus.ts
@@ -3,58 +3,68 @@ import { Plugin } from "../lib/api/plugin";
 import { Logger } from "../lib/logger";
 import { Server } from "../lib/server";
 import path from "path";
-import fs from "fs";
-
-const logger = new Logger("NodePolus", "info");
-
-Error.stackTraceLimit = 25;
-
-logger.info("Starting NodePolus");
-logger.info("Loading config.json");
-
-const serverConfig: ServerConfig = JSON.parse(fs.readFileSync(path.join(__dirname, "config.json"), "utf-8"));
+import fs from "fs/promises";
 
 declare const server: Server;
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-(global as any).server = new Server(serverConfig);
+(async (): Promise<void> => {
+  const logger = new Logger("NodePolus", "info");
 
-logger.info("Loading plugins");
+  Error.stackTraceLimit = 25;
 
-const pathToPlugins = path.join(__dirname, "./plugins/");
-const pluginDirectories = fs.readdirSync(pathToPlugins);
-const plugins: Plugin[] = [];
+  logger.info("Starting NodePolus");
+  logger.info("Loading config.json");
 
-for (let i = 0; i < pluginDirectories.length; i++) {
-  const pathToPlugin = path.join(pathToPlugins, pluginDirectories[i]);
+  try {
+    const serverConfig: ServerConfig = JSON.parse(await fs.readFile(path.join(__dirname, "config.json"), "utf-8"));
 
-  if (path.extname(pathToPlugin).toLowerCase() !== ".npplugin") {
-    logger.warn(`Skipping folder "${pluginDirectories[i]}" as it does not end with ".npplugin"`);
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    (global as any).server = new Server(serverConfig);
 
-    continue;
+    logger.info("Loading plugins");
+
+    const pathToPlugins = path.join(__dirname, "./plugins/");
+    const pluginDirectories = await fs.readdir(pathToPlugins);
+    const plugins: Plugin[] = [];
+
+    for (let i = 0; i < pluginDirectories.length; i++) {
+      const pathToPlugin = path.join(pathToPlugins, pluginDirectories[i]);
+
+      if (path.extname(pathToPlugin).toLowerCase() !== ".npplugin") {
+        logger.warn(`Skipping folder "${pluginDirectories[i]}" as it does not end with ".npplugin"`);
+
+        continue;
+      }
+
+      logger.debug(`Loading ${path.basename(pathToPlugin)}`);
+
+      const plugin: Plugin = {
+        folder: pluginDirectories[i],
+        metadata: JSON.parse(await fs.readFile(path.join(pathToPlugin, "plugin.json"), "utf-8")),
+      };
+
+      await import(path.join(pathToPlugin, "index"));
+
+      plugins.push(plugin);
+      logger.info(`Loaded plugin: ${plugin.metadata.name} v${plugin.metadata.version.join(".")}`);
+    }
+
+    server.listen().then(() => {
+      logger.info(`Server listening on ${server.getAddress()}:${server.getPort()}`);
+
+      server.emit("server.ready");
+    });
+
+    process.on("uncaughtException", err => {
+      logger.catch(err);
+    });
+
+    // process.on("unhandledRejection", (event: PromiseRejectionEvent) => {
+    //   logger.catch(event.reason instanceof Error ? event.reason : new Error(event.reason));
+    // });
+  } catch (error) {
+    logger.catch(error);
+
+    process.exit(1);
   }
-
-  logger.debug(`Loading ${path.basename(pathToPlugin)}`);
-
-  const plugin: Plugin = {
-    folder: pluginDirectories[i],
-    metadata: JSON.parse(fs.readFileSync(path.join(pathToPlugin, "plugin.json"), "utf-8")),
-  };
-
-  plugins.push(plugin);
-  logger.info(`Loaded plugin: ${plugin.metadata.name} v${plugin.metadata.version.join(".")}`);
-}
-
-server.listen().then(() => {
-  logger.info(`Server listening on ${server.getAddress()}:${server.getPort()}`);
-
-  server.emit("server.ready");
-});
-
-process.on("uncaughtException", err => {
-  logger.catch(err);
-});
-
-// process.on("unhandledRejection", (event: PromiseRejectionEvent) => {
-//   logger.catch(event.reason instanceof Error ? event.reason : new Error(event.reason));
-// });
+})();
