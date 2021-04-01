15a91852518deab62fa4c761be69f49cc5ae0e01
Rose Hall <rose@hall.ly>
2021-03-31 22:17:21 -0400

    Add latest version to allowed versions constant

--- src/util/constants.ts
+++ src/util/constants.ts
@@ -5,7 +5,7 @@ import { ServerConfig } from "../api/config";
  * A list of all Among Us client versions supported by NodePolus.
  */
 export const SUPPORTED_VERSIONS: readonly ClientVersion[] = [
-  new ClientVersion(2021, 3, 5),
+  new ClientVersion(2021, 3, 25),
 ];
 
 /**
